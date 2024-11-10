56ea8769237a16faf0926fc3d9c3d6f808476035
kskkoushik <kskkoushik135@gmail.com>
2024-11-10 17:45:33 +0530

    added allowed cors origin

--- appointment_project/settings.py
+++ appointment_project/settings.py
@@ -56,7 +56,9 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://safe-github-dld2.vercel.app",  # Replace with your actual Vercel URL
+]
 
 ROOT_URLCONF = "appointment_project.urls"
 
