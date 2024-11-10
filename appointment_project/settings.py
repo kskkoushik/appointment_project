82843b58c64b83a13c06ce04229f02173033afd9
kskkoushik <kskkoushik135@gmail.com>
2024-11-10 18:29:20 +0530

    I have set the debug to true again

--- appointment_project/settings.py
+++ appointment_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-^+olgt2v^sshc5$#q43=br9pef^#9l2%!8017fmd&9##*2qh=n"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "https://appointment-project-0vlk.onrender.com",
